3735e9cdd3fce294977ce17b395d83e23baf48ce
Sanum Sheikh <szsheikh@andrew.cmu.edu>
2015-02-22 23:54:09 -0500

    more changes

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ActiveRecord::Base
-  belongs_to :category
-
-  validates_presence_of :title
-  validates_presence_of :context
-  scope :active, where('active = ?', true)
-  scope :alphabetical, order('title')
-end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :photos
-  has_many :articles
   
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
