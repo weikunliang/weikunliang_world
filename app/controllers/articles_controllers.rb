c0662ab43893e7f7fb6d1a2b8feb0e62a039a1be
Sanum Sheikh <szsheikh@andrew.cmu.edu>
2015-02-23 02:05:55 -0500

    fixed controlllers and views

--- app/controllers/articles_controllers.rb
+++ app/controllers/articles_controllers.rb
@@ -1,16 +1,19 @@
 class ArticlesController < ApplicationController
   
   def index
+
     @articles = Article.alphabetical.paginate(:page => params[:page]).per_page(10)
   end
 
 
   def show
+
     @article = Article.find(params[:id])
   end
 
 
   def new
+
     @article = Article.new
   end
 
