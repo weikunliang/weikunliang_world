3b2ec5788c716c17c5754d59aedf38632666b6ac
Sanum Sheikh <szsheikh@andrew.cmu.edu>
2015-02-23 01:47:57 -0500

    created articles model and fixed relationshps

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
+  
   belongs_to :category
 
   validates_presence_of :title
+  
   validates_presence_of :context
   scope :active, where('active = ?', true)
   scope :alphabetical, order('title')
